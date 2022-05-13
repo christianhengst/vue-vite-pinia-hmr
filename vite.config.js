4146c69eac7a7eff54191a3ee70e5ab4cda573bc
Christian Hengst <buero@christianhengst.de>
2022-05-13 08:45:12 +0200

    removes "css" from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 
 export default defineConfig(({ command, mode }) => {
-
   return {
-
-    // inject some global scss files
-    css: {
-      preprocessorOptions: {
-        scss: {
-          charset: false,
-        },
-      },
-    },
-
-    // add plugins
-    plugins: [
-
-      vue(),
-
-    ],
+    plugins: [vue()],
   };
 });
